28d55426b1f5726ff1c5a2d11ee09be1601107c8
ALEJANDRITO\Alejandro hdez g <alejandro14fe@msn.com>
2017-03-31 15:46:51 -0600

    Subi proyecto

---
+++ webresponsive/index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Web responsive</title>
+	   <meta charset=utf-8>
+       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/fontello.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+	<link rel="stylesheet" type="text/css" href="css/menu.css">
+	<link rel="stylesheet" type="text/css" href="css/banner.css">
+	<link rel="stylesheet" type="text/css" href="css/blog.css">
+	<link rel="stylesheet" type="text/css" href="css/info.css">
+</head>
+<body>
+<header>
+	<div class="contenedor">
+		<h1 class="icon-videocam">Video e Imagen</h1>
+		<input type="checkbox" name="" id="menu-bar">
+		<label class="icon-th-list" for="menu-bar"></label>
+		<nav class="menu">
+			<a href="">Inicio</a>
+			<a href="">Camara</a>
+			<a href="">Video</a>
+			<a href="">Imágenes</a>
+			
+		</nav>
+	</div>
+
+</header>
+<main>
+	<section id="banner">
+		<img src="imagenes/Graduación-ITTG.jpg">
+		<div class="contenedor"> 
+		<h2>Fotografía y video Profesional</h2>
+		<p>Paquetes a tu medida y Diseños elegantes</p>
+		<a href="#">Leer más</a>
+	</div>
+
+	</section>
+		<section id="bienvenida">
+		<div class="contenedor">
+			<h2>BIENVENIDO </h2>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias praesentium quo, sunt numquam optio dignissimos explicabo delectus dolore earum nobis quae recusandae quidem labore voluptas! Voluptate vero commodi, provident aspernatur.</p>	
+	</div>	
+		</section>
+		<section id="blog">
+			<h3>Lo más nuevo</h3>
+			<div class="contenedor">
+
+				<article>
+					<img src="imagenes/quince2.jpg" >
+					<h3>Eventos de Quince Años</h3>
+				</article>
+				<article>
+					<img src="imagenes/traje.jpg" >
+					<h3>Fotos de Trámite</h3>
+				</article>
+				<article>
+					<img src="imagenes/quince.jpg" >
+					<h3>Fiestas y Reuniones</h3>
+				</article>
+				<article>
+					<img src="imagenes/graduacion.jpg" >
+					<h3> Paquetes de Graduaciones</h3>
+				</article>
+			</div>
+
+		</section>
+		<section id="info">
+			<h3>Recordar es volver a vivir , ese es nuestro objetivo</h3>
+		<div class="contenedor">
+			<div class="info-foto">
+				<img src="imagenes/img1.jpg" >
+				<h4>Eventos</h4>
+			</div>
+			<div class="info-foto">
+				<img src="imagenes/img2.jpg" >
+				<h4>Graduaciones</h4>
+			</div>
+			<div class="info-foto">
+				<img src="imagenes/img3.jpg" >
+				<h4>Fiestas</h4>
+			</div>
+			<div class="info-foto">
+				<img src="imagenes/img4.jpg" >
+				<h4>Foto y Video</h4>
+			</div>
+
+			
+		</div>
+		</section>
+	</main>
+
+		<footer>
+			<div class="contenedor">
+				<p class="copy">Foto y Video &copy; 2017</p>
+				<div class="sociales">
+					<a class="icon-facebook" href="#"></a>
+					<a class="icon-youtube" href="#"></a>
+					<a class="icon-twitter"	href="#"></a>
+					<a class="icon-instagram" href="#"></a>
+				</div>
+			</div>
+		</footer>
+
+</body>
+</html>
